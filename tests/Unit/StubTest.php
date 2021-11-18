82955eff2e1160a857ce97a0ba76d9ef4257316f
Artem <FiammaTheGreat@ya.ru>
2021-11-19 02:58:59 +0300

    Create StubTest.php

---
+++ tests/Unit/StubTest.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace Tests\Unit;
+
+use PHPUnit\Framework\TestCase;
+
+class StubTest extends TestCase
+{
+    //apparently github actions workflow that i use breaks
+    //if there's no test here
+}
