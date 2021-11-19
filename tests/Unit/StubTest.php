b614887a13c3dc31573a1e345fea47e7814aab51
Artem <FiammaTheGreat@ya.ru>
2021-11-19 03:02:29 +0300

    Update StubTest.php
    Last one to make workflow nice and clean

--- tests/Unit/StubTest.php
+++ tests/Unit/StubTest.php
@@ -8,4 +8,8 @@ class StubTest extends TestCase
 {
     //apparently github actions workflow that i use breaks
     //if there's no test here
+    public function test() {
+        $this->assertTrue(true); //if you are looking over here for some reason
+                                          //there is nothing to be found here
+    }
 }
