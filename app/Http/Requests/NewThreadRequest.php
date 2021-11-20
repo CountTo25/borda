dadfb15f7a5eb3928855d5a1f3d085bdc68990b5
Artem <FiammaTheGreat@ya.ru>
2021-11-20 23:51:23 +0300

    tests

--- app/Http/Requests/NewThreadRequest.php
+++ app/Http/Requests/NewThreadRequest.php
@@ -18,7 +18,7 @@ class NewThreadRequest extends FormRequest
             'board_id' => ['required', 'exists:boards,id'],
             'content' => ['string', 'min:3', 'max:2048', 'nullable'],
             'user_name' => ['string', 'min:1', 'max:100', 'nullable'],
-            'images' => ['nullable', 'array'],
+            'images' => ['nullable', 'array', 'max:3'],
             'images.*' => ['mimes:jpeg,jpg,png', 'max:4096'],
         ];
     }
